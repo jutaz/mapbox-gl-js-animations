1aa7b5bc85592618a140f0ee18bb5397173ee0dd
Justas Brazauskas <brazauskasjustas@gmail.com>
2019-11-02 23:34:29 +0200

    eslint set-up

--- src/index.js
+++ src/index.js
@@ -1,44 +1,42 @@
-import anime from 'animejs/lib/anime.es.js';
+import anime from 'animejs/lib/anime.es.js'
 
 function runAnimation (map) {
-  const animations = [];
+  const animations = []
 
   map.getStyle().layers.forEach((layer) => {
     if (!layer.metadata || !Array.isArray(layer.metadata.animations)) {
-      return;
+      return
     }
 
-    layer.metadata.animations.forEach(({rules, variables, paint}) => {
-      const vars = {...variables};
-      const paintEntries = Object.entries(paint);
-      const varKeys = Object.keys(vars);
+    layer.metadata.animations.forEach(({ rules, variables, paint }) => {
+      const vars = { ...variables }
+      const paintEntries = Object.entries(paint)
+      const varKeys = Object.keys(vars)
 
       const varDefs = (expression) => {
-        const def = [];
-        let ref = def;
+        const def = []
+        let ref = def
 
         if (varKeys.length === 0) {
-          return expression;
+          return expression
         }
 
         for (var i = 0; i < varKeys.length; i++) {
-          varKeys[i]
-
           if (def.length === 0) {
-            ref.push('let', varKeys[i], ['literal', vars[varKeys[i]]]);
-            continue;
+            ref.push('let', varKeys[i], ['literal', vars[varKeys[i]]])
+            continue
           }
 
-          const expr = ['let', varKeys[i], ['literal', vars[varKeys[i]]]];
+          const expr = ['let', varKeys[i], ['literal', vars[varKeys[i]]]]
 
-          ref.push(expr);
+          ref.push(expr)
 
-          ref = expr;
+          ref = expr
         }
 
         ref.push(expression)
 
-        return def;
+        return def
       }
 
       animations.push({
@@ -46,43 +44,46 @@ function runAnimation (map) {
           targets: vars,
           ...vars,
           ...rules,
+          // Always force autoplay to `false`.
           autoplay: false,
           update: () => {
             for (var i = 0; i < paintEntries.length; i++) {
-              const [prop, expression] = paintEntries[i];
+              const [prop, expression] = paintEntries[i]
               map.setPaintProperty(layer.id, prop, varDefs(expression), {
                 validate: false
-              });
+              })
             }
           }
         })
       })
     })
-  });
+  })
 
-  let currentTask;
+  let currentTask
 
   function loop () {
-    const t = performance.now();
+    const t = window.performance.now()
     for (var i = 0; i < animations.length; i++) {
-      animations[i].anime.tick(t);
+      animations[i].anime.tick(t)
     }
-    currentTask = map._renderTaskQueue.add(loop);
+    currentTask = map._renderTaskQueue.add(loop)
   }
 
-  currentTask = map._renderTaskQueue.add(loop);
+  currentTask = map._renderTaskQueue.add(loop)
 
   map.on('webglcontextlost', () => map._renderTaskQueue.remove(currentTask))
-  map.on('webglcontextrestored', () => currentTask = map._renderTaskQueue.add(loop))
+  map.on('webglcontextrestored', () => {
+    currentTask = map._renderTaskQueue.add(loop)
+  })
 }
 
 function addToMap (map) {
   if (map.loaded()) {
-    return runAnimation(map);
+    return runAnimation(map)
   }
 
   map.on('load', () => runAnimation(map))
 }
 
-export default {addToMap}
-export {addToMap}
+export default { addToMap }
+export { addToMap }
